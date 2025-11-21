5d14efc481c6b7921a5bd8284ffc4e0a1c2e52ad
[dyad] <randy@cvlcvc.com>
2025-11-21 16:48:54 -0500

    [dyad] Whitelist image domain in Next.js config - wrote 1 file(s)

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.pravatar.cc",
+      },
+    ],
+  },
   webpack: (config) => {
     if (process.env.NODE_ENV === "development") {
       config.module.rules.push({
